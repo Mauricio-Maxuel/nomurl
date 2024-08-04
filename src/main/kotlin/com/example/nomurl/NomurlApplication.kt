c7bfb8307929b11ed2a29d75b7e763b06f036fbf
Mauricio-Maxuel <mauricio.msantos@sptech.school>
2024-08-04 12:43:01 -0300

    initial commit

---
+++ src/main/kotlin/com/example/nomurl/NomurlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nomurl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NomurlApplication
+
+fun main(args: Array<String>) {
+	runApplication<NomurlApplication>(*args)
+}
